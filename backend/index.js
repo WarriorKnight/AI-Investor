5c0006802811336d74412f04579f109876d546cb
Vojtěch Škopek <vojtba@gmail.com>
2025-04-08 11:24:40 +0200

    Update database initialization and action loading logic

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
-const { fetchAllStocks } = require("./stocks/stocks.js");
-const {predict} = require('./predict.js');
+// const { fetchAllStocks } = require("./stocks/stocks.js");
+// const {predict} = require('./predict.js');
+const{initializeDatabase} = require('./db/client.js')
 const {loadActions} = require('./actions.js');
+
+
 (async () => {
     // const output = await fetchAllStocks();
     // console.log(output);
     // console.log(predict(output));
-    loadActions('[{  "action": "BUY",  "symbol": "AAPL",  "quantity": 10,  "reason": "Apples stock is undervalued based on recent earnings."},{  "action": "SELL",  "symbol": "TSLA",  "quantity": 5,  "reason": "Teslas stock has reached a short-term high and is overvalued."},{  "action": "BUY",  "symbol": "MSFT",  "quantity": 15,  "reason": "Microsoft shows strong growth potential in cloud computing."}]')
+    await initializeDatabase(25000);
+
+    loadActions('[{  "action": "SELL",  "symbol": "AAPL",  "quantity": 10,  "reason": "Apples stock is undervalued based on recent earnings."},{  "action": "SELL",  "symbol": "TSLA",  "quantity": 5,  "reason": "Teslas stock has reached a short-term high and is overvalued."},{  "action": "BUY",  "symbol": "MSFT",  "quantity": 15,  "reason": "Microsoft shows strong growth potential in cloud computing."}]')
 })();
